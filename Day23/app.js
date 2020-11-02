b20ffe0562d440fb5a0b4d5bbe5e170c18d7b345
SihamBen <siham.bendaia37@gmail.com>
2020-11-02 16:45:17 +0100

    added day23

---
+++ Day23/app.js
@@ -0,0 +1,34 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+
+const avatar = document.querySelector(".avatar img");
+const info1 = document.querySelector(".info1");
+const info2 = document.querySelector(".info2");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  avatar.style.transform = "translateZ(140px) rotateZ(-5deg)";  
+  info1.style.transform = "translateZ(75px)";
+  info2.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  avatar.style.transition = "all 0.8s ease";
+  info1.style.transition = "all 1s ease";
+  info2.style.transition = "all 1.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+  avatar.style.transform = "translateZ(0px) rotateZ(0deg)";
+  info1.style.transform = "translateZ(0px)";
+  info2.style.transform = "translateZ(0px)";
+});
--- README.md
+++ README.md
@@ -22,3 +22,4 @@ I challenged myself to create beautiful UI elements inspired by UI Design Daily 
 - Day 20: A reponsive web app
 - Day 21: Just another Dashboard
 - Day 22:An outdoor activities website
+- Day 23: 3d animated card
